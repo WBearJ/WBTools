46bffb761d130e5284d87bc7d84a56c772352a64
WBearJ <karuasha@gmail.com>
2022-04-13 16:28:40 +0800

    add func

--- Example/WBTools/ViewController.swift
+++ Example/WBTools/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import WBTools
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        WBHUDTool.showCircle()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ WBArchivableTool.swift
@@ -0,0 +1,93 @@
+//
+//  WBArchivableTool.swift
+//  WBTools
+//
+//  Created by WBear on 2022/4/13.
+//
+
+import Foundation
+
+protocol WBArchivable {
+    var archive: NSDictionary { get }
+    init?(unarchive: NSDictionary?)
+}
+
+struct WBArchivableTool {
+    static func documentDirtory(_ file: String) -> String? {
+        //document文件夹
+        guard let documentDir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .allDomainsMask, true).first else {
+            return nil
+        }
+        return documentDir + "/" + file
+    }
+    
+    //MARK: ============= objects archive
+    
+    @discardableResult
+    static func archiveObjects<T: WBArchivable>(lists: [T], file: String) -> Bool {
+        guard let archivePath = documentDirtory(file) else {
+            return false
+        }
+        
+        let encodedLists = lists.map{ $0.archive }
+        do {
+            let data = try? NSKeyedArchiver.archivedData(withRootObject: encodedLists, requiringSecureCoding: false)
+            try data?.write(to: URL(fileURLWithPath: archivePath))
+            return true
+        }catch {
+            print(error)
+            return false
+        }
+    }
+
+    static func unarchiveObjects<T: WBArchivable>(file: String) -> [T]? {
+        guard let archivePath = documentDirtory(file) else {
+            return nil
+        }
+        
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: archivePath))
+            let list = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data)
+            guard let decodedLists = list as? [NSDictionary] else {
+                return nil
+            }
+            return decodedLists.compactMap { return T(unarchive: $0) }
+        }catch {
+            return nil
+        }
+    }
+    
+    //MARK: ================= object archive
+    @discardableResult
+    static func archiveObject<T: WBArchivable>(object: T, file: String) -> Bool {
+        guard let archivePath = documentDirtory(file) else {
+            return false
+        }
+        
+        do {
+            let data = try? NSKeyedArchiver.archivedData(withRootObject: object.archive, requiringSecureCoding: false)
+            try data?.write(to: URL(fileURLWithPath: archivePath))
+            return true
+        }catch {
+            print(error)
+            return false
+        }
+    }
+
+    static func unarchiveObject<T: WBArchivable>(file: String) -> T? {
+        guard let archivePath = documentDirtory(file) else {
+            return nil
+        }
+        
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: archivePath))
+            let object = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data)
+            guard let jsonDic = object as? NSDictionary else {
+                return nil
+            }
+            return T(unarchive: jsonDic)
+        }catch {
+            return nil
+        }
+    }
+}
